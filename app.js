ebd35485511f0bcb1b99241b85ad8be35a97a1d6
NickTheo <NickTheo@gemail.com>
2021-10-27 14:30:34 +0300

    basic strature basic code minus

---
+++ app.js
@@ -0,0 +1,34 @@
+let life = 100;
+
+const name = 'Nick';
+
+
+const updatedLife = () =>{
+
+
+
+}
+
+//life =life - 10;
+const damage = () => {
+    if(life > 0){
+    life = life - 10;
+    console.log(life)
+    //console.clear();
+    }else if(life = 0){
+        
+    } 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+

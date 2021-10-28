739d4f6ec26ecc5394bbba4fce1cd582e74db040
NickTheo <NickTheo@gemail.com>
2021-10-28 12:02:47 +0300

    full healButton working, full damageButton working, full shieldButton working, added person but den sinxronizonat ta data den xerw giati otan pas 0 life den mporei na healaris allo gt exeis pets pethani

--- app.js
+++ app.js
@@ -1,24 +1,68 @@
-let life = 100;
-
-const name = 'Nick';
-
-
-const updatedLife = () =>{
-
-
+function project1() {
 
 }
+
+let startinglife = 100;
+let startingshield = 100;
+
+//const name = 'Nick';
+
+const person = [
+    name = "Nick",
+    life = startinglife,
+    shield = startingshield
+]
 
 //life =life - 10;
 const damage = () => {
-    if(life > 0){
-    life = life - 10;
-    console.log(life)
-    //console.clear();
-    }if(life <= 0){
-        alert("you are dead");
+    if (shield != 0){
+        shield = shield - 10;
+        console.log(name, life, shield);
+    }else{
+        if(life > 0){
+            life = life - 10;
+            console.log(name, life, shield)
+            //console.clear();
+            }
+            
+            if(life <= 0){
+                alert("you are dead");
+            } 
+    }
+
+
+}
+
+const heal = () => {
+    if(life > 1){
+        life = life + 10;
+        console.log(name, life, shield)    
+    }
+    
+    if (life > 100){
+        alert("you already have max health");
+        life = life -10; 
+        console.log(name, life, shield);
     } 
 }
+
+const extrashield = () => {
+    if(shield > 1){
+        shield = shield + 10;
+        console.log(name, life, shield)    
+    }
+    
+    if (shield > 100){
+        alert("you already have max shield");
+        shield = shield -10; 
+        console.log(name, life, shield);
+    }  
+}
+
+console.log(person)
+
+
+
 
 
 
